06a2763f45fcf773763c790c466379ba81ce2803
Jideofor <njusticej@gmail.com>
2020-04-28 16:45:15 +0100

    feat: connect to reducer store and handle dispatch

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,49 @@
-import React from 'react';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { addRecipe, removeFromCalendar } from "../actions";
 
-function App() {
-  return (
-    <div className="">
-      <header className="">
-      <h1>hello world</h1>
-      </header>
-    </div>
-  );
+class App extends Component {
+  doThing = () => {
+    this.props.selectRecipe({})
+  }
+  render() {
+    console.log("props", this.props);
+    return (
+      <div className="">
+        <header className="">
+          <h1>hello world</h1>
+        </header>
+      </div>
+    );
+  }
 }
 
-export default App;
+function mapStateToProps(calendar) {
+  const dayOrder = [
+    "sunday",
+    "monday",
+    "tuesday",
+    "wednesday",
+    "thursday",
+    "friday",
+    "saturday",
+  ];
+  return {
+    calendar: dayOrder.map((day) => ({
+      day,
+      meals: Object.keys(calendar[day]).reduce((meals, meal) => {
+        meals[meal] = calendar[day][meal] ? calendar[day][meal] : null;
+        return meals;
+      }, {}),
+    })),
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    selectRecipe: (data) => dispatch(addRecipe(data)),
+    remove: (data) => dispatch(removeFromCalendar(data)),
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
