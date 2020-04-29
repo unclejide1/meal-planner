cc181e86b1d7065ebe40b83eaf95c4633a3d6b19
Jideofor <njusticej@gmail.com>
2020-04-29 21:39:24 +0100

    feat: wrap parent component with provider

--- src/index.js
+++ src/index.js
@@ -5,17 +5,19 @@ import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 import {createStore} from 'redux';
 import reducer from './reducers';
-
+import {Provider} from 'react-redux';
 
 const store = createStore(
   reducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
-console.log(store.getState()) 
+
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store = {store}>
     <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
